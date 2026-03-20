20411cd1b7c736bd083c78d9ad92dc115ece6eb9
Bertrussoff <bertramoff11@gmail.com>
2026-03-20 03:56:58 +0000

    Update message to version 2.0

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from DevOps Lab! Version 2.0 🚀'
+    return 'Hello from DevOps Lab! Version 2.0.1 🚀'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
